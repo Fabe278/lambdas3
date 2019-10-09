594c782f3f69a3ceba4f2caa7507a3a24ed17c08
fhainzinger17 <fabian.irger3@gmail.com>
2019-10-09 07:51:49 +0200

    Aufgabe 5_2 done

--- src/uebung5_2/BiConsumerBiPredicateBiFunction.java
+++ src/uebung5_2/BiConsumerBiPredicateBiFunction.java
@@ -28,9 +28,9 @@ public class BiConsumerBiPredicateBiFunction {
         biAnzeige((Integer i1, Integer i2) ->
         System.out.println("Das Ergebnis des Testes " + i2 + " * 2 ==" + i1 + " mit BiPredicate ist " + result2), 15, 5);
         
-//        boolean result3 = vergleich(, 2);
-//        biAnzeige((Integer i1, Integer i2) ->
-//        System.out.println("Das Ergebnis des Testes " + i2 + " * 2 ==" + i1 + " mit BiPredicate ist " + result2), 15, 5);
+        boolean result3 = vergleich(i -> i==254, 254);
+        anzeige((Integer i) ->
+        System.out.println("Das Ergebnis des Testes " + i + " mit vergleich mit 254 ist " + result2), 254);
         
         Integer result6 = biBerechnung((Integer i1, Integer i2) -> i1 + i2, 5, 10);
         biAnzeige((Integer i1, Integer i2) ->
@@ -39,6 +39,10 @@ public class BiConsumerBiPredicateBiFunction {
         Integer result7 = biBerechnung((Integer i1, Integer i2) -> i1 + i2, 5, 15);
         biAnzeige((Integer i1, Integer i2) ->
         System.out.println("Der Wert der BiFunction mit den Argumenten " + i1 + " und "+i2+" ist "+ result7),5,15);
+        
+        Integer result8 = berechnung((Integer i) -> i*i*i, 10);
+        anzeige((Integer i) -> 
+        System.out.println("Der Wert der Berechnung mit " + i + "^3 ist " + result8),10);
         
         BinaryOperator<Integer> maxByOperator = BinaryOperator.<Integer> maxBy(Integer::compareTo);
         BinaryOperator<Integer> minByOperator = BinaryOperator.<Integer> minBy((v1,v2) -> Integer.compare(v1, v2));
@@ -56,6 +60,9 @@ public class BiConsumerBiPredicateBiFunction {
         anzeige(i ->
         System.out.println("Der Wert des UnaryOperators mit dem Argument "+ i + " ist " +result11),5);
         
+        Integer result12 = berechnungmitBinaryOperator((Integer i1, Integer i2) -> i1+i2, 5, 10);
+        biAnzeige((Integer i1, Integer i2) ->
+        System.out.println("Der Wert des BerechnungMitBinaryOperators mit " +i1+ " + " + i2+ " ist " + result12), 5,10);
     }
     
     public static boolean vergleich(Predicate<Integer> predicate, Integer i){
